545ba04865b90a5d91935d57d0e0e7cfbffa88de
Andre Funk <andre.funk@icloud.com>
2026-01-21 16:34:10 +0100

    style: aside mobile-nav added

--- src/templates/cart-additional.js
+++ src/templates/cart-additional.js
@@ -3,15 +3,15 @@ import { formatToCurrency } from "../utils/utlis"
 export function renderCartAdditionalHTML(cart) {
     return `
         <div class="cart__subtotal">
-            <p>subtotal</p>
+            <p>Zwischensumme</p>
             <p>${formatToCurrency(cart.total - cart.deliveryFee)}</p>
         </div>
         <div class="cart__delivery-fee">
-            <p>Delivery Fee</p>
+            <p>Lieferung</p>
             <p>${formatToCurrency(cart.deliveryFee)}</p>
         </div>
         <div class="cart__total">
-            <p>total</p>
+            <p>Gesamt</p>
             <p>${formatToCurrency(cart.total)}</p>
         </div>
         <button class="cart__pay-btn" data-cart-checkout>Bezahlen (${formatToCurrency(cart.total)})</button> 
--- src/templates/dish.js
+++ src/templates/dish.js
@@ -14,7 +14,7 @@ export function getDishHTML(dish) {
 
         <div class="dish__additional" data-dish-additional>
             <p class="dish__price">${formatToCurrency(dish.price)}</p>
-            <button class="dish__add-btn" data-add-to-cart=${dish.id}>Add to Cart</button>
+            <button class="dish__add-btn" data-add-to-cart=${dish.id}>Hinzufügen</button>
         </div>
     </article>
     `
